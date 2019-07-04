4f1f9a825743e620c53be511ad1a3620e1b53bc2
Caleb McKay <cmckay7@liberty.edu>
2019-07-04 19:24:02 -0400

    Initial commit. Configure webpack and nodemon for development.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './front/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    port: 8080
+  }
+};
